e4f1a3b52462a7a9fa36567f0f12fe3b5281a442
Ayneg-Kyiv <134535856+Ayneg-Kyiv@users.noreply.github.com>
2023-06-02 01:28:45 +0300

    Added bunch of new features that include optional additional information to user structure, independent add/delete functione for this information significant changes in some other functions.

--- ConsoleApplication65/Header.h
+++ ConsoleApplication65/Header.h
@@ -6,12 +6,13 @@ struct User
 	char Phone[11];
 	char* Email = nullptr;
 	int Info_Size;
-	char** Info;
+	char** Info=nullptr;
 };
 
 void Read(User*&, int&);
 
 void Add(User*&, int&);
+void AddInfo(User*&, int, int);
 
 int NameCheck(char*);
 int PhoneCheck(char[11]);
@@ -23,6 +24,7 @@ void Show(User*, int, int);
 void Change(User*&, int, int);
 
 void Del(User*&, int&, int);
+void DelInfo(User*&, int, int);
 
 void Sort(User*&, int);
 
